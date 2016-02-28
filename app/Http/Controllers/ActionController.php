1cdd1dd277eb8c88d15338374971c809fb45df2e
maps90 <dimas.abdulaziiz@mataharimall.com>
2016-02-28 21:56:13 +0700

    added simple page for presentation

--- app/Http/Controllers/ActionController.php
+++ app/Http/Controllers/ActionController.php
@@ -13,6 +13,7 @@ class ActionController extends Controller
     {
         $message =  $request->input('msg');
         $momo = new Momo('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkZXZpY2VfaWQiOiI0MzI0MzI0MzI0MzI0MjQiLCJleHBpcmVkX3RpbWUiOiIyMDE2LTAzLTI4IDE5OjUzOjAwIiwicmVmcmVzaF90b2tlbiI6IjFjNDdhNmFkMWEwMjRjZmY1ODk5YmFlMzUwNTU1OTRhIiwiY2xpZW50X2lkIjoiYW5kcm9pZF9waG9uZSIsImNsaWVudF9zZWNyZXQiOiJmZDFjZTUzOTJkMmM2MzFiNzg5MWRlMzdjM2QyYzUyZCJ9._hp9yo_w3HRbpklwl6V_iAiQp_hq3PcLwVV-JkY8_v8', $message);
-        return $momo->result();
+        $result = $momo->result();
+        return view('action.sms', compact('result'));
     }
 }
---
+++ resources/views/action/sms.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <title>MOMO MESSAGE</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/css/bootstrap.min.css" rel="stylesheet"></link>
+        <link href="/css/animate.css" rel="stylesheet"></link>
+        <link href="/css/fonts.css" rel="stylesheet"></link>
+        <link href="/css/main.css" rel="stylesheet"></link>
+    </head>
+    <body>
+        <section class="content-section" id="content">
+            <div class="container">
+                <div class="row">
+                    <div class="cntr">
+                        <div class="hands">
+                            <img class="hands" src="../images/hands.png" />
+                            <div class="text"><?php echo $result; ?></div>
+                        </div>
+                    </div>
+                     <img class="logo-mm" src="../images/Logo-MatahariMall.png"/>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
